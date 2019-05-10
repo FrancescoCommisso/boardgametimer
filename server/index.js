1f6d65bcd73a00ab906a2c2c73b90ad87338bc9c
Francesco Commisso <francescocommissodev@gmail.com>
2019-05-10 14:22:12 -0400

    changed serve path

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(pino);
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
