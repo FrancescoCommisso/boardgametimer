5d6d39ba8cb72e98cfa16900dc74515846e2b338
Francesco Commisso <francescocommissodev@gmail.com>
2019-05-10 14:23:24 -0400

    changed serve path

--- server/index.js
+++ server/index.js
@@ -9,15 +9,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 app.use(pino);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static('../client/build'));
+  app.use(express.static("../client/build"));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
 
 var games = {};
 
